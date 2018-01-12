9c0e4c0cbbe5db49d95f828c8d9af09318d88ac0
Morven Lewis-Everley <morven@i-lateral.com>
2018-01-12 18:04:16 +0000

    Add function to get related controller frrom current object

--- src/extensions/CatalogueLinkExtension.php
+++ src/extensions/CatalogueLinkExtension.php
@@ -4,11 +4,15 @@ namespace SilverCommerce\CatalogueFrontend\Extensions;
 
 use SilverStripe\ORM\DataExtension;
 use SilverStripe\Forms\FieldList;
+use SilverStripe\Core\ClassInfo;
 use SilverStripe\Control\Controller;
 use SilverStripe\Control\Director;
+use SilverStripe\Core\Injector\Injector;
+use SilverStripe\Dev\Deprecation;
 use SilverStripe\View\Parsers\URLSegmentFilter;
 use SilverStripe\CMS\Forms\SiteTreeURLSegmentField;
 use SilverStripe\CMS\Model\SiteTree;
+use SilverCommerce\CatalogueFrontend\Control\CatalogueController;
 use SilverCommerce\CatalogueAdmin\Model\CatalogueProduct;
 use SilverCommerce\CatalogueAdmin\Model\CatalogueCategory;
 
@@ -67,10 +71,49 @@ class CatalogueLinkExtension extends DataExtension
         );
     }
 
+    /**
+     * Find the controller name by our convention of {$ModelClass}Controller
+     *
+     * @return string
+     */
+    public function getControllerName()
+    {
+        //default controller for SiteTree objects
+        $controller = CatalogueController::class;
+
+        //go through the ancestry for this class looking for
+        $ancestry = ClassInfo::ancestry($this->owner->ClassName);
+
+        // loop over the array going from the deepest descendant (ie: the current class) to SiteTree
+        while ($class = array_pop($ancestry)) {
+            //we don't need to go any deeper than the SiteTree class
+            if ($class == CatalogueProduct::class || $class == CatalogueCategory::class) {
+                break;
+            }
+            
+            // If we have a class of "{$ClassName}Controller" then we found our controller
+            if (class_exists($candidate = sprintf('%sController', $class))) {
+                $controller = $candidate;
+                break;
+            } elseif (class_exists($candidate = sprintf('%s_Controller', $class))) {
+                // Support the legacy underscored filename, but raise a deprecation notice
+                Deprecation::notice(
+                    '5.0',
+                    'Underscored controller class names are deprecated. Use "MyController" instead of "My_Controller".',
+                    Deprecation::SCOPE_GLOBAL
+                );
+                $controller = $candidate;
+                break;
+            }
+        }
+
+        return $controller;
+    }
+
     public function onBeforeWrite()
     {
         // Only call on first creation, ir if title is changed
-        if ($this->isChanged('Title') || !$this->owner->URLSegment) {
+        if ($this->owner->isChanged('Title') || !$this->owner->URLSegment) {
             // Set the URL Segment, so it can be accessed via the controller
             $filter = URLSegmentFilter::create();
             $t = $filter->filter($this->owner->Title);
