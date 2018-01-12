80133e732fcfd0b27784e95de31cbb26bd66ee11
Morven Lewis-Everley <morven@i-lateral.com>
2018-01-12 18:05:06 +0000

    Switch from overloading routing to swapping ModelAsController via Injector

--- src/control/ModelAsController.php
+++ src/control/ModelAsController.php
@@ -4,7 +4,10 @@ namespace SilverCommerce\CatalogueFrontend\Control;
 
 use SilverStripe\Core\ClassInfo;
 use SilverStripe\Core\Injector\Injector;
+use SilverStripe\ORM\DataObject;
+use SilverStripe\CMS\Model\SiteTree;
 use SilverStripe\CMS\Controllers\ModelAsController as CMSModelAsController;
+use SilverCommerce\CatalogueAdmin\Model\CatalogueCategory;
 
 /**
  * Customise default @link ModelAsController to allow for finding and setting
@@ -18,40 +21,19 @@ class ModelAsController extends CMSModelAsController
      * {@link CatalogueProductController} for handling the relevent
      * object.
      *
-     * @param $object Either Product or Category object
+     * @param $object A {@link DataObject} with the getControllerName() method
      * @param string $action
      * @return CatalogueController
      */
-    protected static function controller_for_object($object, $action = null)
+    public static function controller_for_object($object, $action = null)
     {
-        if ($object->class == 'CatalogueProduct') {
-            $controller = "CatalogueProductController";
-        } elseif ($object->class == 'CatalogueCategory') {
-            $controller = "CatalogueCategoryController";
-        } else {
-            $ancestry = ClassInfo::ancestry($object->class);
-            
-            while ($class = array_pop($ancestry)) {
-                if (class_exists($class . "_Controller")) {
-                    break;
-                }
-            }
-            
-            // Find the controller we need, or revert to a default
-            if ($class !== null) {
-                $controller = "{$class}_Controller";
-            } elseif (ClassInfo::baseDataClass($object->class) == "CatalogueProduct") {
-                $controller = "CatalogueProductController";
-            } elseif (ClassInfo::baseDataClass($object->class) == "CatalogueCategory") {
-                $controller = "CatalogueCategoryController";
-            }
-        }
+        $controller = $object->getControllerName();
 
         if ($action && class_exists($controller . '_' . ucfirst($action))) {
             $controller = $controller . '_' . ucfirst($action);
         }
-        
-        return class_exists($controller) ? Injector::inst()->create($controller, $object) : $object;
+
+        return Injector::inst()->create($controller, $object);
     }
 
     /**
@@ -72,12 +54,25 @@ class ModelAsController extends CMSModelAsController
         }
 
         // Select child page
-        $conditions = array('"SiteTree"."URLSegment"' => rawurlencode($URLSegment));
+        $sitetree_conditions = ["URLSegment" => rawurlencode($URLSegment)];
+        $cat_conditions = $sitetree_conditions;
+        $product_conditions = $sitetree_conditions;
+        
         if (SiteTree::config()->get('nested_urls')) {
-            $conditions[] = array('"SiteTree"."ParentID"' => 0);
+            $sitetree_conditions['ParentID'] = 0;
         }
-        /** @var SiteTree $sitetree */
-        $sitetree = DataObject::get_one(SiteTree::class, $conditions);
+
+        $cat_conditions['ParentID'] = 0;
+
+        $object = SiteTree::get()
+            ->filter($sitetree_conditions)
+            ->first();
+
+        if (!$object) {
+            $object = CatalogueCategory::get()
+                ->filter($cat_conditions)
+                ->first();
+        }
 
         // Check translation module
         // @todo Refactor out module specific code
@@ -85,19 +80,19 @@ class ModelAsController extends CMSModelAsController
             Translatable::enable_locale_filter();
         }
 
-        if (!$sitetree) {
+        if (!$object) {
             $this->httpError(404, 'The requested page could not be found.');
         }
 
         // Enforce current locale setting to the loaded SiteTree object
-        if (class_exists('Translatable') && $sitetree->Locale) {
-            Translatable::set_current_locale($sitetree->Locale);
+        if (class_exists('Translatable') && $object->Locale) {
+            Translatable::set_current_locale($object->Locale);
         }
 
         if (isset($_REQUEST['debug'])) {
-            Debug::message("Using record #$sitetree->ID of type " . get_class($sitetree) . " with link {$sitetree->Link()}");
+            Debug::message("Using record #$object->ID of type " . get_class($object) . " with link {$sitetree->Link()}");
         }
 
-        return self::controller_for($sitetree, $this->getRequest()->param('Action'));
+        return self::controller_for_object($object, $this->getRequest()->param('Action'));
     }
 }
